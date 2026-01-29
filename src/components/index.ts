9807954ad0bef2d9d4c796320daf742291361f56
Arnav K <arnavkaushal09@gmail.com>
2026-01-29 12:50:03 +0530

    slop initial (#4)

---
+++ src/components/index.ts
@@ -0,0 +1,10 @@
+export { Editor, useEditor, loader } from "./Editor"
+export type {
+  EditorProps,
+  EditorFile,
+  EditorTheme,
+} from "./Editor"
+export type { editor, languages } from "./Editor"
+
+export { CodeEditor } from "./CodeEditor"
+export type { CodeEditorProps } from "./CodeEditor"
